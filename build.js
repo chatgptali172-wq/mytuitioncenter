c491e63df18fd487ba86e32d31bf00d9e1b8ffac
muhammadnumanlatif <muhammadnumanlatif@gmail.com>
2026-07-19 01:35:43 +0500

    Remove API_BASE from source completely

--- build.js
+++ build.js
@@ -8,9 +8,9 @@ let content = fs.readFileSync(indexPath, 'utf8');
 const apiBase = process.env.API_BASE;
 
 if (apiBase) {
-  content = content.replace(/const API_BASE = '[^']+';/g, `const API_BASE = '${apiBase}';`);
+  content = content.replace('<!-- ROUTING & API SCRIPT -->\n  <script>', `<!-- ROUTING & API SCRIPT -->\n  <script>\n    const API_BASE = '${apiBase}';`);
   fs.writeFileSync(indexPath, content);
   console.log(`[Build] Successfully injected API_BASE: ${apiBase}`);
 } else {
-  console.log('[Build] No API_BASE environment variable found. Using hardcoded URL.');
+  console.log('[Build] No API_BASE environment variable found. API calls will fail unless defined globally.');
 }
