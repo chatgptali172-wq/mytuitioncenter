452b61b596634bef00f8e553696bdb6daaea556d
muhammadnumanlatif <muhammadnumanlatif@gmail.com>
2026-07-19 01:33:16 +0500

    Clean up API_BASE injection code

--- build.js
+++ build.js
@@ -7,9 +7,9 @@ let content = fs.readFileSync(indexPath, 'utf8');
 const apiBase = process.env.API_BASE;
 
 if (apiBase) {
-  content = content.replace(/\{\{API_BASE\}\}/g, apiBase);
+  content = content.replace(/const API_BASE = '[^']+';/g, `const API_BASE = '${apiBase}';`);
   fs.writeFileSync(indexPath, content);
   console.log(`[Build] Successfully injected API_BASE: ${apiBase}`);
 } else {
-  console.log('[Build] No API_BASE environment variable found. Defaulting to local/hardcoded URL.');
+  console.log('[Build] No API_BASE environment variable found. Using hardcoded URL.');
 }
